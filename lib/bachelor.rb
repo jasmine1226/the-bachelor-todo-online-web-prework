ca3e94870613ce4ab98a207ee800e3595a65a137
Jasmine Chen <jasmine1226@gmail.com>
2019-05-20 05:25:08 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -56,7 +56,7 @@ def get_average_age_for_season(data, season)
   age_sum = 0
   data[season].each do |contestant|
       count += 1
-      age_sum = age_sum + contestant["age"].to_i
+      age_sum = age_sum + contestant["age"].to_f
   end
   average = age_sum / count
 end
