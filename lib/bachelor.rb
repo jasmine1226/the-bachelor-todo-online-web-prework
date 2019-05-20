e2d1100cd3d28b722bfd84e9fff5f64a89eacc76
Jasmine Chen <jasmine1226@gmail.com>
2019-05-20 05:08:02 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,9 +1,25 @@
 def get_first_name_of_season_winner(data, season)
   # code here
+  winner = ""
+  data[season].each do |contestant|
+    if contestant.fetch("status") == "Winner"
+      winner = contestant["name"].split(" ")[0]
+    end
+  end
+  winner
 end
 
 def get_contestant_name(data, occupation)
   # code here
+  name = ""
+  data.each do |season, contestants|
+    contestants.each do |contestant|
+      if contestant.fetch("occupation") == occupation
+        name = contestant["name"]
+      end
+    end
+  end
+  name
 end
 
 def count_contestants_by_hometown(data, hometown)
